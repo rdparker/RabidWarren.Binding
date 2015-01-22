9954bad84b18449fb26333299e446728dea5413a
Ron Parker <rdparker@gmail.com>
2015-01-21 21:08:15 -0500

    Start creating unit tests

--- RabidWarren.Binding.Tests/Properties/AssemblyInfo.cs
+++ RabidWarren.Binding.Tests/Properties/AssemblyInfo.cs
@@ -1,28 +1,26 @@
-﻿// -----------------------------------------------------------------------
-//  <copyright file="AssemblyInfo.cs" company="Ron Parker">
-//   Copyright 2014 Ron Parker
-//  </copyright>
-//  <summary>
-//   Defines the Binding assembly.
-//  </summary>
-// -----------------------------------------------------------------------
-using System.Reflection;
-using System.Resources;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Binding")]
+[assembly: AssemblyTitle("RabidWarren.Binding.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Binding")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("RabidWarren.Binding.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-[assembly: InternalsVisibleTo("RabidWarren.Binding.Tests")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b421c3f2-fc6d-481d-b053-5a33f510e7ee")]
 
 // Version information for an assembly consists of the following four values:
 //

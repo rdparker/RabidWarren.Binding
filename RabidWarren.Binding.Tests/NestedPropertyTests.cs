b973157594288edfa6e8c8cae7ac4eef27fe14ed
Ron Parker <rdparker@gmail.com>
2015-02-01 16:20:31 -0500

    Add a basic nested property test

---
+++ RabidWarren.Binding.Tests/NestedPropertyTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabidWarren.Binding.Tests
+{
+    using System.ComponentModel;
+    using NUnit.Framework;
+
+    class NestedPropertyTests : BindingObject
+    {
+        public string Text { get; set; }
+
+        [Test]
+        public void BasicNestedProperty()
+        {
+            var nested = new Nested();
+
+            Bind(this, "Text", nested, "OuterProperty.Inner");
+        }
+
+        class Nested : INotifyingObject
+        {
+            public Outer OuterProperty { get; set; }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public void OnPropertyChangedEvent(string propertyName)
+            {
+                throw new NotImplementedException();
+            }
+
+            public class Outer
+            {
+                public string Inner;
+            }
+        }
+    }
+}

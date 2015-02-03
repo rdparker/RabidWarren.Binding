5570cb687896de6fa4337fe6d7acbf769a650c1c
Ron Parker <rdparker@gmail.com>
2015-02-02 20:18:32 -0500

    Remove the unnecessary IntegerConverter from Tests
    There is already one in RabidWarren.Binding.

--- RabidWarren.Binding.Tests/BindingConverterTests.cs
+++ RabidWarren.Binding.Tests/BindingConverterTests.cs
@@ -45,29 +45,6 @@
             }
         }
 
-        [BindingConverter(typeof(int), typeof(string))]
-        public class IntegerConverter : BindingConverter
-        {
-            public override object ConvertTo(object value, Type targetType, object parameter)
-            {
-                var n = (int)value;
-                return n.ToString();
-            }
-   
-            public override object ConvertFrom(object value, Type targetType, object parameter)
-            {
-                var strValue = value as string;
-                int result;
-
-                if (int.TryParse(strValue, out result))
-                {
-                    return result;
-                }
-
-                return NoValue;
-            }
-        }
-
         View _view;
         ViewModel _viewModel;
 

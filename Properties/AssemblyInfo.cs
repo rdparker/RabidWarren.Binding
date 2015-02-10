aa3b79cda4c138177c8866d5b5a52ad0f572d5b3
Ron Parker <rdparker@gmail.com>
2015-02-09 19:20:36 -0500

    Add a NuGet package definition
    Update the AssemblyInfo and build setting for generating a NuGet package.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -13,12 +13,12 @@ using System.Runtime.CompilerServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Binding")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("RabidWarren.Binding")]
+[assembly: AssemblyDescription("A Property binding system implemented in a Portable Class Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Binding")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Ron Parker")]
+[assembly: AssemblyProduct("RabidWarren.Binding")]
+[assembly: AssemblyCopyright("Copyright ©  2014, 2015 Ron Parker")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -34,5 +34,4 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]

416b9c943746175ce6ca4627cd7bdb566bef7adf
Ron Parker <rdparker@gmail.com>
2015-03-07 18:03:10 -0500

    Add tests for expression-related exceptions

---
+++ RabidWarren.Binding.Tests/Expressions/ExpressionExceptions.cs
@@ -0,0 +1,47 @@
+﻿namespace RabidWarren.Binding.Tests.Expressions
+{
+    using System;
+    using NUnit.Framework;
+
+    class ExpressionExceptions
+    {
+        View _view;
+        ViewModel _viewModel;
+
+        [SetUp]
+        public void Setup()
+        {
+            _view = new View();
+            _viewModel = new ViewModel();
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException),
+            ExpectedMessage = "Unsupported method call 'Dummy' in expression")]
+        public void UnsupportedMethodCall()
+        {
+            _view.Bind(v => v.Text, _viewModel, vm => Dummy());
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException),
+            ExpectedMessage = "Unsupported expression type: 'Add'")]
+        public void UnsupportedExpression()
+        {
+            _view.Bind(v => v.Text, _viewModel, vm => "Hi, " + vm.Text);
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException),
+            ExpectedMessage = "The CanWrite method should never be called directly.")]
+        public void CannotCallCanWrite()
+        {
+            _view.Text.CanWrite();
+        }
+
+        static string Dummy()
+        {
+            return "Dummy";
+        }
+    }
+}

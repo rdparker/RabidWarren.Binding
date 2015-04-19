b08fca6bcc2094912890e4cbe49fd8f6373dff65
Ron Parker <rdparker@gmail.com>
2015-04-19 14:36:27 -0400

    Visith the child expression, not the parent
    Instead of trying to splice the child onto references to the parent, use the more typical composition pattern of replacing references to the child's parameters, with a reference to the parent.

--- CompositionVisitor.cs
+++ CompositionVisitor.cs
@@ -1,22 +1,20 @@
 ﻿namespace RabidWarren.Binding
 {
     using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
     using System.Linq.Expressions;
-    using System.Reflection;
 
     class CompositionVisitor<TGrandparent, TParent, TChild> : ExpressionVisitor
     {
-        private Expression<Func<TGrandparent, TParent>> _parent;
-        private MemberInfo parentMember;
-        private Expression<Func<TParent, TChild>> _child;
-        private Expression childBody;
+        Expression<Func<TGrandparent, TParent>> Parent { get; set; }
+        Expression<Func<TParent, TChild>> Child { get; set; }
 
         CompositionVisitor(Expression<Func<TGrandparent, TParent>> parent, Expression<Func<TParent, TChild>> child)
         {
-            _parent = parent;
-            parentMember = (parent.Body as MemberExpression).Member;
-            _child = child;
-            childBody = child.Body;
+            Parent = parent;
+            Child = child;
         }
 
         public static Expression<Func<TGrandparent, TChild>> Visit(
@@ -24,37 +22,100 @@
         {
             var visitor = new CompositionVisitor<TGrandparent, TParent, TChild>(parent, child);
 
-            return (Expression<Func<TGrandparent, TChild>>)visitor.Visit(parent);
+            return (Expression<Func<TGrandparent, TChild>>)visitor.Visit(child);
         }
 
         protected override Expression VisitLambda<T>(Expression<T> node)
         {
-            if (object.ReferenceEquals(node, _parent))
-                return Expression.Lambda(Visit(node.Body), VisitAndConvert(node.Parameters, "VisitLambda"));
+            if (ReferenceEquals(node, Child))
+                return Expression.Lambda(Visit(Child.Body), VisitAndConvert(Parent.Parameters, "VisitLambda"));
 
             return base.VisitLambda<T>(node);
         }
 
         protected override Expression VisitMember(MemberExpression node)
         {
-            if (object.ReferenceEquals(node.Member, parentMember))
+            var childBody = Child.Body;
+
+            if (ReferenceEquals(node, childBody) && childBody.NodeType == ExpressionType.MemberAccess)
+                return Expression.MakeMemberAccess(Parent.Body, (childBody as MemberExpression).Member);
+
+            return base.VisitMember(node);
+        }
+
+        /// <summary>
+        /// Visits the children of the MethodCallExpression, composing any references to <c>Child</c> with
+        /// <c>Parent</c>.
+        /// </summary>
+        /// <param name="node">The expression to visit.</param>
+        /// <returns>
+        /// The modified expression, if it or any subexpression was modified; otherwise, returns the original expression.
+        /// </returns>
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            var args = ChainParameters(Parent.Body, node.Arguments);
+            
+            args = VisitExpressionList(args);
+
+            var methodInfo = node.Method;
+            if (methodInfo.IsStatic)
+                return Expression.Call(null, methodInfo, args);
+
+            return Expression.Call(args[0], methodInfo, args.Skip(1));
+        }
+
+        /// <summary>
+        /// Replaces any parameter usage in the expression list with <paramref name="parent"/>, effectively chaining
+        /// or composing the two.
+        /// </summary>
+        /// <param name="parent">
+        /// The parent expression to use in replacing naked parameters in the expression list.
+        /// </param>
+        /// <param name="arguments">
+        /// A list of Expressions, which make up the arguments of a method call.
+        /// </param>
+        /// <returns>
+        /// The modified expression list, if any element or subexpression was modified; otherwise returns the original
+        /// expression list.
+        /// </returns>
+        private ReadOnlyCollection<Expression> ChainParameters(Expression parent, ReadOnlyCollection<Expression> arguments)
+        {
+            if (arguments.All(x => x.NodeType != ExpressionType.Parameter))
+                return arguments;
+
+            var newArgs = arguments.Select(x => x.NodeType == ExpressionType.Parameter ? parent : x);
+
+            return new ReadOnlyCollection<Expression>(newArgs.ToList());
+        }
+
+        /// <summary>
+        /// Visits the children of the Expression list.
+        /// </summary>
+        /// <param name="original">The list of Expressions to visit.</param>
+        /// <returns>
+        /// The modified expression list, if any element or subexpression was modified; otherwise, returns the
+        /// original expression list.
+        /// </returns>
+        protected virtual ReadOnlyCollection<Expression> VisitExpressionList(ReadOnlyCollection<Expression> original)
+        {
+            List<Expression> list = null;
+            for (int i = 0, n = original.Count; i < n; i++)
             {
-                switch (childBody.NodeType)
+                Expression p = Visit(original[i]);
+                if (list != null)
+                    list.Add(p);
+                else if (p != original[i])
                 {
-                    case ExpressionType.MemberAccess:
-                        return Expression.MakeMemberAccess(node, (childBody as MemberExpression).Member);
-
-                    case ExpressionType.Call:
-                        var c = Expression.Call((childBody as MethodCallExpression).Method, node);
-                        return c;
-
-                    default:
-                        throw new NotSupportedException(
-                            string.Format("Unsupported expression type: '{0}'", childBody.NodeType));
+                    list = new List<Expression>(n);
+                    list.AddRange(original.Take(i - 1));
+                    list.Add(p);
                 }
             }
 
-            return base.VisitMember(node);
+            if (list != null)
+                return new ReadOnlyCollection<Expression>(list);
+
+            return original;
         }
     }
 }

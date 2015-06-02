0fc40a9bc2faa1d41272cf6269842f33e8067061
Ron Parker <rdparker@gmail.com>
2015-06-01 20:14:56 -0400

    Add a test for a bug
    There was a problem calling ConverterRegistry.RegisterAllConverters directly on an assembly.

---
+++ RabidWarren.Binding.Tests.ConverterAssembly/TestConverter.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="TestConverter.cs" company="Ron Parker">
+//   Copyright 2015 Ron Parker
+//  </copyright>
+//  <summary>
+//   A converter for testing ConverterRegistry.RegisterAllConverters().
+//  </summary>
+// -----------------------------------------------------------------------
+
+namespace RabidWarren.Binding.Tests.ConverterAssembly
+{
+    using System;
+
+    /// <summary>
+    /// This is for testing the ChickenAndEgg bug in ConverterRegistry.RegisterAllConverters.
+    /// </summary>
+    [BindingConverter(typeof(TestConverter), typeof(object))]
+    public class TestConverter : BindingConverter
+    {
+        public override object ConvertTo(object value, Type targetType, object parameter)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override object ConvertFrom(object value, Type targetType, object parameter)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- RabidWarren.Binding.Tests/BindingConverterTests.cs
+++ RabidWarren.Binding.Tests/BindingConverterTests.cs
@@ -1,7 +1,8 @@
 ﻿namespace RabidWarren.Binding.Tests
 {
-    using NUnit.Framework;
     using System;
+    using NUnit.Framework;
+    using RabidWarren.Binding.Tests.ConverterAssembly;
 
 
     /// <summary>
@@ -50,22 +51,35 @@
 
         [SetUp]
         public void Setup()
-        {
+        {   
             _view = new View();
             _viewModel = new ViewModel { Number = 314 };
+        }
 
-            _view.Bind(_view, "Number", _viewModel, "Number");
+        /// <summary>
+        /// There was a bug with a chicken and egg issue, where a call to
+        /// <see cref="M:ConverterRegistry.RegisterAllConverters"/> would fail when invoked directly for an assembly.
+        /// This is because the RegisterAllConverters method relied upon _registry having been constructed and by
+        /// default it was only constructed when the Registry property was gotten, which also caused
+        /// RegisterAllConverters to be invoked on the assembly that contains ConverterRegistry.
+        /// </summary>
+        [Test]
+        public void ChickenAndEgg()
+        {
+            ConverterRegistry.RegisterAllConverters(typeof(TestConverter).Assembly); 
         }
 
         [Test]
         public void ConvertToTarget()
         {
+            _view.Bind(_view, "Number", _viewModel, "Number");
             Assert.AreEqual("314", _view.Number);
         }
 
         [Test]
         public void ConvertBackToSource()
         {
+            _view.Bind(_view, "Number", _viewModel, "Number");
             _view.Number = "278";
 
             Assert.AreEqual(278, _viewModel.Number);

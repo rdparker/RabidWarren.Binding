1bd9bac6c97ee0f260635fa3de7c67c14766fb62
Ron Parker <rdparker@gmail.com>
2015-07-19 18:10:00 -0400

    Clean up extension methods

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -16,27 +16,26 @@ using System.Linq.Expressions;
 namespace RabidWarren.Binding
 {
     /// <summary>
-    /// Contains binding-related extension methods.
+    ///     Contains binding-related extension methods.
     /// </summary>
-    public static class BindingObjectExtensions
+    public static class ExtensionMethods
     {
         /// ////////////////////////////////////////////////////////////////////////////////////////////////
         /// <summary>
-        /// Binds a target object property to a source object property using expressions.
+        ///     Binds a target object property to a source object property using expressions.
         /// </summary>
-        /// <remarks> 
-        /// This is used when a binding is established outside of the target objects code.  Such as happens
-        /// when a WPF Window makes a call to bind a View property to a View Model property. </remarks>
+        /// <remarks>
+        ///     This is used when a binding is established outside of the target objects code.  Such as happens
+        ///     when a WPF Window makes a call to bind a View property to a View Model property.
+        /// </remarks>
         /// <typeparam name="TTarget">      The type of the target object. </typeparam>
         /// <typeparam name="TTargetValue"> The type of the target property. </typeparam>
         /// <typeparam name="TSource">      The type of the source object. </typeparam>
         /// <typeparam name="TSourceValue"> The type of the source property. </typeparam>
         /// <param name="target">           The target object. </param>
-        /// <param name="targetProperty">   An expression from the target object to the target
-        ///                                 property. </param>
+        /// <param name="targetProperty">   An expression from the target object to the target property.</param>
         /// <param name="source">           The source object. </param>
-        /// <param name="sourceProperty">   An expression from the source object to the source
-        ///                                 property. </param>
+        /// <param name="sourceProperty">   An expression from the source object to the source property.</param>
         /// ////////////////////////////////////////////////////////////////////////////////////////////////
         public static void Bind<TTarget, TTargetValue, TSource, TSourceValue>(
             this TTarget target,
@@ -53,14 +52,16 @@ namespace RabidWarren.Binding
         }
 
         /// <summary>
-        /// Does nothing.  This method is used to check whether or not a property can be read when using property
-        /// binding expressions.  However, it is never really called because the binding mechanism replaces it with
-        /// a constant value.
+        ///     Does nothing.  This method is used to check whether or not a property can be read when using property
+        ///     binding expressions.  However, it is never really called because the binding mechanism replaces it with
+        ///     a constant value.
         /// </summary>
         /// <typeparam name="TProperty">    The type of the property</typeparam>
         /// <param name="property"> The property whose writable state is being checked. </param>
         /// <exception cref="InvalidOperationException"> Thrown if this function is called at runtime. </exception>
         /// <returns>Nothing.  Calling this function at runtime will result in an exception. </returns>
+        [SuppressMessage("ReSharper", "UnusedParameter.Global",
+            Justification = "The function is never called, but a this pointer is required.")]
         public static bool CanWrite<TProperty>(this TProperty property)
         {
             throw new InvalidOperationException("The CanWrite method should never be called directly.");
@@ -68,8 +69,8 @@ namespace RabidWarren.Binding
 
         /// ////////////////////////////////////////////////////////////////////////////////////////////////
         /// <summary>
-        /// Gets the name of a member specified by an expression.  This can be used for property binding
-        /// expressions or for passing around a property name without risking a mismatch in a string.
+        ///     Gets the name of a member specified by an expression.  This can be used for property binding
+        ///     expressions or for passing around a property name without risking a mismatch in a string.
         /// </summary>
         /// <param name="expression">   The expression that specifies the member. </param>
         /// <returns>                   The member name. </returns>
@@ -79,10 +80,10 @@ namespace RabidWarren.Binding
             switch (expression.NodeType)
             {
                 case ExpressionType.Lambda:
-                    return GetMemberName(((LambdaExpression)expression).Body);
+                    return GetMemberName(((LambdaExpression) expression).Body);
 
                 case ExpressionType.MemberAccess:
-                    MemberExpression me = (MemberExpression)expression;
+                    var me = (MemberExpression) expression;
                     var parentExpression = me.Expression;
 
                     if (parentExpression.NodeType == ExpressionType.Constant ||
@@ -94,7 +95,7 @@ namespace RabidWarren.Binding
                     return parentExpression.GetMemberName() + "." + me.Member.Name;
 
                 case ExpressionType.Call:
-                    var call = (MethodCallExpression)expression;
+                    var call = (MethodCallExpression) expression;
                     var name = call.Method.Name;
 
                     // Handle the CanWrite pseudo-property.
@@ -116,13 +117,14 @@ namespace RabidWarren.Binding
         }
 
         /// <summary>
-        /// Gets the value of an object's property given a property expression.
+        ///     Gets the value of an object's property given a property expression.
         /// </summary>
         /// <typeparam name="TObject">   The type of the object.</typeparam>
         /// <typeparam name="TProperty"> The type of its property.</typeparam>
         /// <param name="obj">           The object.</param>
-        /// <param name="property">      The property expression.  These are generally of the form
-        ///                              <code>() => Property</code>.</param>
+        /// <param name="property">
+        ///     The property expression.  These are generally of the form <code>() => Property</code>.
+        /// </param>
         /// <returns>                    The property value.</returns>
         [SuppressMessage("ReSharper", "MemberCanBePrivate.Global", Justification = "This is part of the bulic API.")]
         public static TProperty GetProperty<TObject, TProperty>(
@@ -131,26 +133,28 @@ namespace RabidWarren.Binding
             where TProperty : class
         {
             var name = property.GetMemberName();
-            var values = from p in typeof(TObject).GetProperties()
+            var values = from p in typeof (TObject).GetProperties()
                 where p.Name == name
                 select p.GetValue(obj, null);
 
-            return (TProperty)values.Single();
+            return (TProperty) values.Single();
         }
 
         /// ////////////////////////////////////////////////////////////////////////////////////////////////
         /// <summary>
-        /// If the value of the property specified by the expression is different than the new value passed in, the
-        /// given setter will be called and the appropriate PropertyChanged notification will be sent.
+        ///     If the value of the property specified by the expression is different than the new value passed in, the
+        ///     given setter will be called and the appropriate PropertyChanged notification will be sent.
         /// </summary>
         /// <typeparam name="TObject">      The type of the object containing the property.</typeparam>
         /// <typeparam name="TProperty">    The properties value type.</typeparam>
         /// <param name="obj">              The object.</param>
-        /// <param name="property">         The property expression.  These are generally of the form 
-        ///                                 <code>() => Property</code>.</param>
+        /// <param name="property">
+        ///     The property expression.  These are generally of the form <code>() => Property</code>.
+        /// </param>
         /// <param name="newValue">         The new value for the property.</param>
-        /// <param name="setter">           The action which stores the property.  Often something like
-        ///                                 <code>v => field = v</code>.</param>
+        /// <param name="setter">
+        ///     The action which stores the property.  Often something like <code>v => field = v</code>.
+        /// </param>
         /// ////////////////////////////////////////////////////////////////////////////////////////////////
         public static void SetProperty<TObject, TProperty>(
             this TObject obj, Expression<Func<TProperty>> property, TProperty newValue, Action<TProperty> setter)

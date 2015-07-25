2eb8a94becffba633d017be88f0ecc6e58e26f33
Ron Parker <rdparker@gmail.com>
2015-07-25 11:31:52 -0400

    Use null propogation and clean up NotifyingObject

--- NotifyingObject.cs
+++ NotifyingObject.cs
@@ -1,12 +1,11 @@
-﻿// -----------------------------------------------------------------------
+﻿// ----------------------------------------------------------------------------------------------------------------
 //  <copyright file="NotifyingObject.cs" company="Ron Parker">
 //   Copyright 2014, 2015 Ron Parker
 //  </copyright>
 //  <summary>
-//   Provides the base implementation for raising the PropertyChanged
-//   event.
+//   Provides the base implementation for raising the PropertyChanged event.
 //  </summary>
-// -----------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------
 
 namespace RabidWarren.Binding
 {
@@ -15,57 +14,45 @@ namespace RabidWarren.Binding
     using System.Linq.Expressions;
 
     /// <summary>
-    /// Provides the base implementation for raising the PropertyChanged event on objects which have properties that
-    /// act as binding sources.
+    ///     Provides the base implementation for raising the PropertyChanged event on objects which have properties
+    ///     that act as binding sources.
     /// </summary>
     public abstract class NotifyingObject : INotifyingObject
     {
         /// <summary>
-        /// Occurs when a property value changes.
+        ///     Occurs when a property value changes.
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
         /// <summary>
-        /// Raises the property changed event.
+        ///     Raises the property changed event.
         /// </summary>
         /// <param name="propertyName">The name of the property that was changed.</param>
         [Obsolete("RabidWarren.Binding now uses RaisePropertyChanged.")]
         public void OnPropertyChangedEvent(string propertyName)
         {
-            // Avoid multithreaded race conditions where PropertyChanged could be updated between the test and
-            // the call.
-            var propertyChangedEvent = PropertyChanged;
-            if (propertyChangedEvent != null)
-            {
-                propertyChangedEvent(this, new PropertyChangedEventArgs(propertyName));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        /// ////////////////////////////////////////////////////////////////////////////////////////////////
         /// <summary>
-        /// Raises the PropertyChanged event for the property named in the property expression.
+        ///     Raises the PropertyChanged event for the property named in the property expression.
         /// </summary>
         /// <typeparam name="TProperty">        The type of the object's property.</typeparam>
-        /// <param name="propertyExpression">   The property expression.  These are generally of the form 
-        ///                                     <code>() => Property</code>.</param>
-        /// ////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <param name="propertyExpression">
+        ///     The property expression.  These are generally of the form <code>() => Property</code>.
+        /// </param>
         public void RaisePropertyChanged<TProperty>(Expression<Func<TProperty>> propertyExpression)
         {
             RaisePropertyChanged(propertyExpression.GetMemberName());
         }
 
-        /// ////////////////////////////////////////////////////////////////////////////////////////////////
         /// <summary>
-        /// Raises the PropertyChanged event for the named property.
+        ///     Raises the PropertyChanged event for the named property.
         /// </summary>
         /// <param name="propertyName">         The property which changed.</param>
-        /// ////////////////////////////////////////////////////////////////////////////////////////////////
         public void RaisePropertyChanged(string propertyName)
         {
-            var handler = PropertyChanged;
-
-            if (handler != null)
-                handler(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }

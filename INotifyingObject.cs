1ce8d4164d8d96b4178f05a2bbf19c26e97e1c00
Ron Parker <rdparker@gmail.com>
2015-07-24 21:28:45 -0400

    Clean up INotifyingObject

--- INotifyingObject.cs
+++ INotifyingObject.cs
@@ -1,48 +1,47 @@
-﻿// -----------------------------------------------------------------------
+﻿// ----------------------------------------------------------------------------------------------------------------
 //  <copyright file="INotifyingObject.cs" company="Ron Parker">
 //   Copyright 2014, 2015 Ron Parker
 //  </copyright>
 //  <summary>
-//   Defines an extended interface for objects that implement
-//   INotifyPropertyChange.
+//   Defines an extended interface for objects that implement INotifyPropertyChange.
 //  </summary>
-// -----------------------------------------------------------------------
-
+// ----------------------------------------------------------------------------------------------------------------
 namespace RabidWarren.Binding
 {
     using System;
     using System.ComponentModel;
+    using System.Diagnostics.CodeAnalysis;
     using System.Linq.Expressions;
 
     /// <summary>
-    /// Raises the PropertyChanged event.
+    ///     Raises the PropertyChanged event.
     /// </summary>
     public interface INotifyingObject : INotifyPropertyChanged
     {
         /// <summary>
-        /// Raises the property changed event event.
+        ///     Raises the property changed event event.
         /// </summary>
         /// <param name="propertyName">The name of the property that changed.</param>
         [Obsolete("RabidWarren.Binding now uses RaisePropertyChanged.")]
         void OnPropertyChangedEvent(string propertyName);
 
-        /// ////////////////////////////////////////////////////////////////////////////////////////////////
         /// <summary>
-        /// Raises the PropertyChanged event for the property named in the property expression.
+        ///     Raises the PropertyChanged event for the property named in the property expression.
         /// </summary>
-        /// <typeparam name="TProperty">        The type of the object's property.</typeparam>
-        /// <param name="propertyExpression">   The property expression.  These are generally of the form
-        ///                                     <code>() => Property</code>.</param>
-        /// ////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <typeparam name="TProperty">The type of the object's property.</typeparam>
+        /// <param name="propertyExpression">
+        ///     The property expression.  These are generally of the form <code>() => Property</code>.
+        /// </param>
         void RaisePropertyChanged<TProperty>(Expression<Func<TProperty>> propertyExpression);
 
-
-        /// ////////////////////////////////////////////////////////////////////////////////////////////////
         /// <summary>
-        /// Raises the PropertyChanged event for the named property.
+        ///     Raises the PropertyChanged event for the named property.
         /// </summary>
-        /// <param name="propertyName">         The property which changed.</param>
-        /// ////////////////////////////////////////////////////////////////////////////////////////////////
+        /// <param name="propertyName">The property which changed.</param>
+        [SuppressMessage("ReSharper", "UnusedMemberInSuper.Global",
+            Justification =
+                "This Is not explicitly used within RabidWarren.Binding, but must be implemented because there are " +
+                "times in user code when it must be raised using a string name.")]
         void RaisePropertyChanged(string propertyName);
     }
 }

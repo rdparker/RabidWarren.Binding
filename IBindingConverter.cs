035c84da7d432f7645f3f05da602db9567dd36f4
Ron Parker <rdparker@gmail.com>
2015-07-25 10:06:28 -0400

    Clean up IBindingConverter

--- IBindingConverter.cs
+++ IBindingConverter.cs
@@ -10,21 +10,31 @@
 namespace RabidWarren.Binding
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
 
     /// <summary>
-    /// Declares the interface binding converter classes must implement.  They must also have the
-    /// <see cref="BindingConverterAttribute"/>.  If they do not
-    /// <see cref="M:ConverterRegistry.RegisterAll()"/> will not find them.
+    ///     Declares the interface binding converter classes must implement.  They must also have the
+    ///     <see cref="BindingConverterAttribute" />.  If they do not
+    ///     <see cref="M:ConverterRegistry.RegisterAll()" /> will not find them.
     /// </summary>
-    /// <remarks>Instead of directly inheriting this interface, classes should be derived from
-    /// <see cref="Binding.BindingConverter"/>.  It provides <see cref="Binding.BindingConverter.NoValue"/>, which is
-    /// used to indicate that a property could not be converted between the two types.  This can happen for example,
-    /// when the user clears a numeric field that is bound to an integer before the enter the desired value.</remarks>
+    /// <remarks>
+    ///     Instead of directly inheriting this interface, classes should be derived from
+    ///     <see cref="Binding.BindingConverter" />.  It provides <see cref="Binding.BindingConverter.NoValue" />,
+    ///     which is used to indicate that a property could not be converted between the two types.  This can happen
+    ///     for example, when the user clears a numeric field that is bound to an integer before the enter the desired
+    ///     value.
+    /// </remarks>
+    [SuppressMessage("ReSharper", "UnusedMemberInSuper.Global",
+        Justification = "ReSharper misses the implementation of this interface possibly due to the use of an " +
+                        "abstract class in the hierarchy.")]
+    [SuppressMessage("ReSharper", "UnusedParameter.Global", 
+        Justification = "ReSharper misses the implementation of this interface possibly due to the use of an " +
+                        "abstract class in the hierarchy.")]
     public interface IBindingConverter
     {
         /// <summary>
-        /// Converts to the target type from the source type specified by the class
-        /// <see cref="Binding.BindingConverterAttribute"/>.
+        ///     Converts to the target type from the source type specified by the class
+        ///     <see cref="Binding.BindingConverterAttribute" />.
         /// </summary>
         /// <returns>The converted value.</returns>
         /// <param name="value">The value to convert.</param>
@@ -33,8 +43,8 @@ namespace RabidWarren.Binding
         object ConvertTo(object value, Type targetType, object parameter);
 
         /// <summary>
-        /// Converts from the target type to the source type specified by the class
-        /// <see cref="Binding.BindingConverterAttribute"/>.
+        ///     Converts from the target type to the source type specified by the class
+        ///     <see cref="Binding.BindingConverterAttribute" />.
         /// </summary>
         /// <returns>The converted value.</returns>
         /// <param name="value">The value to convert.</param>
